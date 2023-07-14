405a256b19228e1af466dfe508a83f6e595f5abd
Kenichi Go Uang <kenichigouang@pop-os.localdomain>
2023-07-14 15:08:35 +0800

    Add exercise rules and requirements

--- src/main/java/ShopPricePrinter.java
+++ src/main/java/ShopPricePrinter.java
@@ -6,5 +6,11 @@ public class ShopPricePrinter {
 
     System.out.println("Rose Pharmacy water is worth P40.");
     System.out.println("Dimsum Break water is worth P50.");
+
+    // TODO: For each shop, print all their products
+
+    // TODO: For each shop, print the most expensive product
+
+    // TODO: For each shop, print the least expensive product
   }
 }
