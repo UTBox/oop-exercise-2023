eabd89c82b2b1cf9e37c3db64f73f4f13ccc5d51
BlankDesu13 <romeo.fermano@synacy.com>
2023-07-17 09:42:37 +0800

    Add Shop class

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Shop {
+public class Shop {
+    private String shopName;
+    private Product[] products;
+
+    public Shop(String shopName, Product[] product) {
+        this.shopName = shopName;
+        this.products = product;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public Product[] getProducts() {
+        return  products;
+    }
+
+    public Product getMostExpensiveProduct() {
+        Product mostExpensive = products[0];
+        for (Product product : products) {
+            if (product.getProductPrice() > mostExpensive.getProductPrice()) {
+                mostExpensive = product;
+            }
+        }
+        return mostExpensive;
+    }
+
+    public Product getLeastExpensiveProduct() {
+        Product leastExpensive = products[0];
+        for (Product product : products) {
+            if (product.getProductPrice() < leastExpensive.getProductPrice()) {
+                leastExpensive = product;
+            }
+        }
+        return leastExpensive;
+    }
 }
