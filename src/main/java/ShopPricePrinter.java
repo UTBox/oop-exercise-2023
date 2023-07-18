b633148e28247598b5966e9b4f2201c4de7b53d2
BlankDesu13 <romeo.fermano@synacy.com>
2023-07-18 15:02:34 +0800

    Added shop products which implements the IProduct

--- src/main/java/ShopPricePrinter.java
+++ src/main/java/ShopPricePrinter.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class ShopPricePrinter {
 
   public static void main(String[] args) {
-
     Product[] mercuryProducts = {
             new Product("Shampoo", 10.99),
             new Product("Soap", 20.50),
@@ -23,9 +22,9 @@ public class ShopPricePrinter {
     };
 
     Shop[] shops = {
-            new Shop("Mercury Drug Store", mercuryProducts),
-            new Shop("Rose Pharmacy", roseProducts),
-            new Shop("Watsons Pharmacy", watsonsProducts)
+            new Shop("Mercury Drug Store", new ShopProducts(mercuryProducts)),
+            new Shop("Rose Pharmacy", new ShopProducts(roseProducts)),
+            new Shop("Watsons Pharmacy", new ShopProducts(watsonsProducts))
     };
 
     Scanner scanner = new Scanner(System.in);
@@ -44,15 +43,16 @@ public class ShopPricePrinter {
         break;
       } else if (choice >= 1 && choice <= shops.length) {
         Shop selectedShop = shops[choice - 1];
+        IProduct products = selectedShop.getProducts();
         System.out.println("\n" + selectedShop.getShopName());
         System.out.println("Products Available: ");
 
-        for (Product product : selectedShop.getProducts()) {
+        for (Product product : products.getProducts()) {
           System.out.println(product.getProductName() + " is worth P" + product.getProductPrice());
         }
 
-        Product mostExpensiveProduct = selectedShop.getMostExpensiveProduct();
-        Product leastExpensiveProduct = selectedShop.getLeastExpensiveProduct();
+        Product mostExpensiveProduct = products.getMostExpensiveProduct();
+        Product leastExpensiveProduct = products.getLeastExpensiveProduct();
 
         System.out.println("Most Expensive Product: " + mostExpensiveProduct.getProductName() + " is worth P" + mostExpensiveProduct.getProductPrice());
         System.out.println("Least Expensive Product: " + leastExpensiveProduct.getProductName() + " is worth P" + leastExpensiveProduct.getProductPrice());
--- src/main/java/ShopProducts.java
+++ src/main/java/ShopProducts.java
@@ -1,2 +1,33 @@
-public class ShopProducts {
+public class ShopProducts implements IProduct{
+    private Product[] products;
+
+    public ShopProducts(Product[] products) {
+        this.products = products;
+    }
+    @Override
+    public Product[] getProducts() {
+        return products;
+    }
+
+    @Override
+    public Product getMostExpensiveProduct() {
+        Product mostExpensiveProduct = products[0];
+        for (int i = 1; i < products.length; i++){
+            if (products[i].getProductPrice() > mostExpensiveProduct.getProductPrice()) {
+                mostExpensiveProduct = products[i];
+            }
+        }
+        return mostExpensiveProduct;
+    }
+
+    @Override
+    public Product getLeastExpensiveProduct() {
+        Product leastExpensiveProduct = products[0];
+        for (int i = 1; i < products.length; i++) {
+            if (products[i].getProductPrice() < leastExpensiveProduct.getProductPrice()) {
+                leastExpensiveProduct = products[i];
+            }
+        }
+        return leastExpensiveProduct;
+    }
 }
