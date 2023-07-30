68c437cf2a9cde2e038ca0362205fe4c2d2077af
presmae <precious.marson@synacy.com>
2023-07-30 21:06:05 +0800

    delete comment line

--- src/main/java/PriceItem.java
+++ src/main/java/PriceItem.java
@@ -1,35 +1,12 @@
 public class PriceItem {
-  private final int badmintonRacquetPrice;
+    private final int badmintonRacquetPrice;
 
-  PriceItem(int badmintonRacquetPrice) {
-    this.badmintonRacquetPrice = badmintonRacquetPrice;
-  }
+    PriceItem(int badmintonRacquetPrice) {
+        this.badmintonRacquetPrice = badmintonRacquetPrice;
+    }
 
-  public int getBadmintonRacquetPrice() {
-    return badmintonRacquetPrice;
-  }
+    public int getBadmintonRacquetPrice() {
+        return badmintonRacquetPrice;
+    }
 
 }
-
-
-//  void tobysBadmintonShopPrice() {
-//
-//    System.out.println("Badminton= 2500");
-//    System.out.println("Shuttlecock = 100");
-//    System.out.print("Badminton Grip = 100");
-//  }
-//
-//  void nikeShopPrice() {
-//    System.out.print("Shoes = 4000");
-//    System.out.println("\nTshirt = 699");
-//    System.out.print("Cap = 999");
-//  }
-//
-//  void adidasShopPrice() {
-//    System.out.print("Jacket = 1299");
-//    System.out.println("\nShoes = 4999");
-//    System.out.println("Shorts = 899");
-//  }
-
-//}
-
