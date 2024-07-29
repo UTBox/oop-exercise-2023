46facb616edd2ea641c043ad88aae5ab21f6f2a2
mendozavincent123 <vincent.mendoza@synacy.com>
2024-07-30 00:12:46 +0800

    add shop interface with 3 shop implementations and printer class

---
+++ src/main/java/FashionBotiqueShop.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class FashionBotiqueShop implements Shop {
+    List<Product> productList = new ArrayList<Product>();
+
+    @Override
+    public void addProduct(Product product) {
+        productList.add(product);
+    }
+
+    @Override
+    public void printAllProducts() {
+        productList.forEach(product -> System.out.println(product));
+    }
+
+    @Override
+    public void printMostExpensiveProduct() {
+        productList.stream()
+                .max(Comparator.comparing(product -> product.getPrice()))
+                .ifPresent(product -> System.out.println(product));
+    }
+
+    @Override
+    public void printLeastExpensiveProduct() {
+        productList.stream()
+                .min(Comparator.comparing(product -> product.getPrice()))
+                .ifPresent(product -> System.out.println(product));
+    }
+}
---
+++ src/main/java/Shop.java
@@ -0,0 +1,6 @@
+public interface Shop {
+    public void addProduct(Product product);
+    public void printAllProducts();
+    public void printMostExpensiveProduct();
+    public void printLeastExpensiveProduct();
+}
