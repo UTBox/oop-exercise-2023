2ff326ddc08138c45f3624085fac18e2679efd84
JuliusF-117 <julius.fabrique@synacy.com>
2024-08-03 13:55:33 +0800

    - Add ShopService class - Add files to .gitignore

---
+++ src/main/java/ShopService.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShopService {
+    private List<Shop> shops;
+
+    public ShopService() {
+        this.shops = new ArrayList<>();
+    }
+
+    public void addShop(Shop shop) {
+        shops.add(shop);
+    }
+
+    public void removeShop(String shopName) {
+        shops.removeIf(shop -> shop.getShopName().equalsIgnoreCase(shopName));
+    }
+}
